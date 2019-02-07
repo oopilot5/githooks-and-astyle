dd98a87dbbc32dcb04a3fbbf2bdf245cf2b42a58
oopilot5 <jason.ljj.dev@gmail.com>
2019-02-07 19:52:55 +0800

    test

---
+++ 2.c
@@ -0,0 +1,38 @@
+int Foo(bool isBar)
+{
+    if (isBar)
+        {
+        bar();
+        return 1;  
+        }
+    else
+          return 0;
+
+    int a=	0;
+    int b=   a;
+
+
+    
+    
+int c=0;
+		
+		
+		int d   =c;
+		log(ERROR,"%s %d %s",
+					a,
+					b,
+					c);
+    switch(a )
+    {
+        case 1:
+            break;
+    case 2 : 
+    break;
+        case   3:
+            break ;
+        default:
+        {
+            break;
+        }
+    }
+}
