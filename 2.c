35a7d1ef4351e43f8d91111e9f51cde3999dc455
oopilot5 <jason.ljj.dev@gmail.com>
2019-02-07 20:47:20 +0800

    test3

--- 2.c
+++ 2.c
@@ -1,38 +1,36 @@
 int Foo(bool isBar)
 {
-    if (isBar)
-        {
+    if (isBar) {
         bar();
-        return 1;  
-        }
+        return 1;
+    }
     else
-          return 0;
+        return 0;
 
-    int a=	0;
+    int a=  0;
     int b=   a;
 
 
-    
-    
-int c=0;
-		
-		
-		int d   =c;
-		log(ERROR,"%s %d %s",
-					a,
-					b,
-					c); 
-    switch(a )
-    {
+
+
+    int c=0;
+
+
+    int d   =c;
+    log(ERROR,"%s %d %s",
+        a,
+        b,
+        c);
+    switch(a ) {
         case 1:
             break;
-    case 2 : 
-    break;
+        case 2 :
+            break;
         case   3:
             break ;
-        default:
-        {
+        default: {
             break;
         }
     }
+
 }
